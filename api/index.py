b962b6eec92c362d8f90edf9fa398fdfa35f946b
Aldian Putra <34766290+MAldianPutra@users.noreply.github.com>
2024-12-06 20:03:43 +0800

    trying switch index

--- api/index.py
+++ api/index.py
@@ -10,19 +10,3 @@
 
 # # Vercel requires an 'app' callable
 # handler = app
-
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-
-    @app.route('/')
-    def hello():
-        return "Hello, World!"
-
-    return app
-
-app = create_app()
-
-# Vercel requires an 'app' callable
-handler = app
