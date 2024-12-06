633194c198fe81ed6262adcef2e784774b87b42c
Aldian Putra <34766290+MAldianPutra@users.noreply.github.com>
2024-12-06 19:34:05 +0800

    fix index

--- api/index.py
+++ api/index.py
@@ -1,3 +1,9 @@
+import sys
+from os.path import dirname, abspath
+
+# Add the parent directory to the system path
+sys.path.append(dirname(dirname(abspath(__file__))))
+
 from app import create_app
 
 app = create_app()
