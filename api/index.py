90dbb82eb117708f0d15874dd3857d3d88b1f042
Aldian Putra <34766290+MAldianPutra@users.noreply.github.com>
2024-12-06 19:45:39 +0800

    test vercel

--- api/index.py
+++ api/index.py
@@ -1,10 +1,26 @@
-import sys
-from os.path import dirname, abspath
+# import sys
+# from os.path import dirname, abspath
 
-# Add the parent directory to the system path
-sys.path.append(dirname(dirname(abspath(__file__))))
+# # Add the parent directory to the system path
+# sys.path.append(dirname(dirname(abspath(__file__))))
 
-from app import create_app
+# from app import create_app
+
+# app = create_app()
+
+# # Vercel requires an 'app' callable
+# handler = app
+
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello():
+        return "Hello, World!"
+
+    return app
 
 app = create_app()
 
